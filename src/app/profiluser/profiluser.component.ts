ed53cd1609101c094c2dd9fdddf88f38374de6f3
aimen laouadi <alaouadi@stagiaire-humanbooster.com>
2024-08-05 10:10:07 +0200

    update fonction delete

--- src/app/profiluser/profiluser.component.ts
+++ src/app/profiluser/profiluser.component.ts
@@ -1,11 +1,9 @@
-import { CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule, FormControl, Validators} from '@angular/forms';
+import { ReactiveFormsModule, FormControl, Validators, FormGroup } from '@angular/forms';
 import { ProfiluserService } from '../shared/servicebusinesscase/profiluserservice/profiluser.service';
-import { ActivatedRoute} from '@angular/router';
-import { Router } from '@angular/router';
-import { FormGroup } from '@angular/forms';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../shared/servicebusinesscase/authentification/authservice.service';
 
 @Component({
   selector: 'app-profiluser',
@@ -19,44 +17,74 @@ export class ProfiluserComponent implements OnInit {
   form: FormGroup = new FormGroup({});
   message: string = '';
   showSuccessMessage: boolean = false;
+  showErrorMessage: boolean = false;
 
-  constructor(private profiluserService: ProfiluserService, private route: ActivatedRoute, private router: Router) { }
+  constructor(
+    private profiluserService: ProfiluserService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
-      this.profiluserService.getUser(params['id']).subscribe(user => {
-        this.user = user;
-        this.form = new FormGroup({
-          username: new FormControl(this.user.username, [Validators.required]),
-          lastname: new FormControl(this.user.lastname, [Validators.required]),
-          firstname: new FormControl(this.user.firstname, [Validators.required]),
-          telephone: new FormControl(this.user.telephone, [Validators.required])
-        });
-      });
+      const userId = +params['id']; // assurez-vous que l'ID est un nombre
+      this.profiluserService.getUser(userId).subscribe(
+        user => {
+          this.user = user;
+          this.form = new FormGroup({
+            username: new FormControl(this.user.username, [Validators.required]),
+            lastname: new FormControl(this.user.lastname, [Validators.required]),
+            firstname: new FormControl(this.user.firstname, [Validators.required]),
+            telephone: new FormControl(this.user.telephone, [Validators.required])
+          });
+        },
+        error => {
+          console.error('Erreur lors de la récupération de l\'utilisateur', error);
+          this.message = 'Erreur lors de la récupération de l\'utilisateur';
+          this.showErrorMessage = true;
+        }
+      );
     });
   }
 
   updateUser(): void {
     if (this.form.valid) {
-      this.profiluserService.updateUser(this.user.id, this.form.value).subscribe(() => {
-        this.message = 'Votre demande a bien été prise en compte, merci!';
-        this.showSuccessMessage = true;
-        setTimeout(() => {
-          this.showSuccessMessage = false;
-        }, 3000);
-      });
+      this.profiluserService.updateUser(this.user.id, this.form.value).subscribe(
+        () => {
+          this.message = 'Votre demande a bien été prise en compte, merci!';
+          this.showSuccessMessage = true;
+          this.showErrorMessage = false;
+          setTimeout(() => {
+            this.showSuccessMessage = false;
+          }, 3000);
+        },
+        error => {
+          console.error('Erreur lors de la mise à jour du profil', error);
+          this.message = 'Erreur lors de la mise à jour du profil';
+          this.showErrorMessage = true;
+        }
+      );
     }
   }
 
   deleteUser(): void {
-    this.profiluserService.deleteUser(this.user.id).subscribe(() => {
-      this.message = 'Votre profil a bien été supprimé, merci!';
-      this.showSuccessMessage = true;
-      setTimeout(() => {
-        this.showSuccessMessage = false;
-        this.router.navigateByUrl('/');
-      }, 3000);
-    });
+    this.profiluserService.deleteUser(this.user.id).subscribe(
+      () => {
+        this.message = 'Votre profil a bien été supprimé, merci!';
+        this.showSuccessMessage = true;
+        this.showErrorMessage = false;
+        setTimeout(() => {
+          this.showSuccessMessage = false;
+          this.router.navigateByUrl('/');
+          this.authService.logout();
+        }, 3000);
+      },
+      error => {
+        console.error('Erreur lors de la suppression du profil', error);
+        this.message = 'Erreur lors de la suppression du profil';
+        this.showErrorMessage = true;
+      }
+    );
   }
-
 }
