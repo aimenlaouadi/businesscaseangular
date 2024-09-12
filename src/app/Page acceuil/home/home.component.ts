5f19fa2f3dceb49240bdff6fb05336e0da220611
aimen laouadi <alaouadi@stagiaire-humanbooster.com>
2024-09-12 10:02:30 +0200

    update

--- src/app/Page acceuil/home/home.component.ts
+++ src/app/Page acceuil/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
+import { CommentcamarcheComponent } from '../commentcamarche/commentcamarche.component';
+import { NosprestationsComponent } from '../nosprestations/nosprestations.component';
+import { AvissclientsComponent } from '../avissclients/avissclients.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, CommentcamarcheComponent, NosprestationsComponent, AvissclientsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
